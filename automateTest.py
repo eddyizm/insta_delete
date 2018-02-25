8bc1f992a12c0b40c6707b6da63203cf0cabaac3
eddyizm hplaptop <eddyizm@me.com>
2018-02-25 14:20:09 -0800

    Tried to automate post but having hard time finding elements.

--- automateTest.py
+++ automateTest.py
@@ -1,5 +1,17 @@
 from selenium import webdriver
+import time
 
+# wait functino
+def stime(seconds):
+    return time.sleep(seconds)
+
+# message to test
+def baseMessage():
+    tstamp = time.strftime("%H:%M:%S")
+    m = "Hello, I am Eduardo and it is "+tstamp+".\n I am a mexican bot, not a russian one"
+    return m
+
+# function to get fb credentials from text file 
 def getFBcredentials(textfile):
     with open(logintext) as f:
         return f.read().splitlines()
@@ -7,8 +19,14 @@ def getFBcredentials(textfile):
 logintext = "C:/Users/cervantes/Downloads/slogin.txt"
 user = getFBcredentials(logintext)
 
+# added options to disable notification pop up
+options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+options.add_experimental_option("prefs",prefs)
+
 #start session
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(chrome_options=options)
+
 browser.get('https://facebook.com')       
 
 assert "Facebook" in browser.title
@@ -18,9 +36,12 @@ elemPass = browser.find_element_by_id("pass")
 btnLogin = browser.find_element_by_id("loginbutton")
 
 elemUser.send_keys(user[0])
+stime(2)
 elemPass.send_keys(user[1])
+stime(5)
 btnLogin.submit()
 
-browser.close()
-# browser = webdriver.Chrome()
-# browser.get('http://seleniumhq.org')    
+# successfully logged in. Let's look for the wall post feature. 
+# no luck. will try again later when Leia stops peeing all over the house.
+
+#browser.close()
