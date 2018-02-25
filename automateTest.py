7f8a7b8a665fc3c191c6246b5d1e8705e38b6dfd
eddyizm hplaptop <eddyizm@me.com>
2018-02-24 23:19:17 -0800

    set up to login to facebook successful, russians watch out!

--- automateTest.py
+++ automateTest.py
@@ -1,4 +1,26 @@
 from selenium import webdriver
 
+def getFBcredentials(textfile):
+    with open(logintext) as f:
+        return f.read().splitlines()
+# get login info
+logintext = "C:/Users/cervantes/Downloads/slogin.txt"
+user = getFBcredentials(logintext)
+
+#start session
 browser = webdriver.Chrome()
-browser.get('http://seleniumhq.org')
+browser.get('https://facebook.com')       
+
+assert "Facebook" in browser.title
+#get login elements 
+elemUser = browser.find_element_by_id("email")
+elemPass = browser.find_element_by_id("pass")
+btnLogin = browser.find_element_by_id("loginbutton")
+
+elemUser.send_keys(user[0])
+elemPass.send_keys(user[1])
+btnLogin.submit()
+
+browser.close()
+# browser = webdriver.Chrome()
+# browser.get('http://seleniumhq.org')    
