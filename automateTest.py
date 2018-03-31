01a4e707769bb0ff10528153adde297181787f8b
eddyizm <eddyizm@gmail.com>
2018-03-30 23:10:42 -0700

    going to try and scrape with selenium

--- automateTest.py
+++ automateTest.py
@@ -3,8 +3,8 @@ import time
 
 # globals
 logintext = "C:/Users/cervantes/Downloads/slogin.txt"
-wtext = "Y:/My Documents/Tools/temp.md"
-
+#wtext = "Y:/My Documents/Tools/temp.md"
+wtext = "C:/Users/eddyizm/Desktop/Work/temp.md"
 # wait function
 def stime(seconds):
     return time.sleep(seconds)
@@ -47,9 +47,25 @@ def LoginToFB():
 def getADP(textfile):
     with open(textfile) as f:
         return f.read().splitlines()
+
+def getFrontlineEdu(textfile):
+    with open(textfile) as f:
+        return f.read().splitlines()
+
 #start session
 browser = webdriver.Chrome()
+browser.get('https://login.frontlineeducation.com/login?signin=eb9838bc56c4c37d308b52ada3dacb6e&productId=ABSMGMT&clientId=ABSMGMT#/login')  
 
+user = getFrontlineEdu(wtext)
+eUser = browser.find_element_by_id('Username')
+ePass = browser.find_element_by_id("Password")
+
+eUser.send_keys(user[86])
+stime(2)
+ePass.send_keys(user[87])
+stime(5)
+
+'''
 browser.get('https://workforcenow.adp.com/public/index.htm')  
 assert "ADP" in browser.title
 
@@ -65,3 +81,4 @@ stime(5)
 btnLogin.submit()
 stime(30)
 browser.close()
+'''
