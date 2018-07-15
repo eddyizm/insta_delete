430d52fc311526c67801102031b75c35654ff2e2
eddyizm <eddyizm@gmail.com>
2018-07-15 14:33:28 -0700

    using bs4 to grab links to posts

--- insta_delete.py
+++ insta_delete.py
@@ -1,10 +1,16 @@
+# -*- coding: UTF-8 -*-
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import time
-import json, requests
+
+
+def WriteToArchive(log, data):
+     with open(log, 'w', encoding= 'utf-8') as f:
+         for d in data:
+             f.write(str(d)+'\n')
 
 # store urls to delete later
 log_path = 'C:/Users/eddyizm/Source/Repos/seleniumTesting/env/media_urls.txt'
@@ -14,20 +20,29 @@ browser = webdriver.Chrome()
 browser.get("https://www.instagram.com/eddyizm")
 # Selenium script to scroll to the bottom, wait 3 seconds for the next batch of data to load, then continue scrolling.  
 # It will continue to do this until the page stops loading new data.
-lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-match=False
-while(match==False):
-        lastCount = lenOfPage
-        time.sleep(3)
-        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount==lenOfPage:
-            match=True
-
+# lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+# match=False
+# while(match==False):
+#         lastCount = lenOfPage
+#         time.sleep(5)
+#         lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+#         if lastCount==lenOfPage:
+#             match=True
 # Now that the page is fully scrolled, grab the source code.
 source_data = browser.page_source
-
 # Throw your source into BeautifulSoup and start parsing!
-bs_data = bs(source_data)
+#bs_data = BeautifulSoup(source_data, "html.parser")
+URLS = []
+soup = BeautifulSoup(source_data, parse_only=SoupStrainer('a'))
+
+for link in BeautifulSoup(source_data, parse_only=SoupStrainer('a')):
+  if link.has_attr('href'):
+    t = link.get('href')
+    if t is not None:
+        URLS.append(t)
+    
+      
+WriteToArchive(log_path, URLS)
 
 def writeToLog(log):
     with open(log, 'w') as g:
@@ -37,4 +52,3 @@ def writeToLog(log):
                 if t is not None:
                     g.write(t +'\n')
 
-#writeToLog(log_path)                    
