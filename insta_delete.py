778a5258ebeb6773b429c5759eb0f982d353524c
eddyizm <eddyizm@gmail.com>
2018-07-17 22:20:01 -0700

    opened login page. tested ok

--- insta_delete.py
+++ insta_delete.py
@@ -6,58 +6,68 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup, SoupStrainer
 import time
 
-def WriteToArchive(log, data):
-     with open(log, 'w', encoding= 'utf-8') as f:
-         for d in data:
-             f.write(str(d)+'\n')
-
 # store urls to delete later
 log_path = 'C:/Users/eddyizm/Source/Repos/seleniumTesting/env/media_urls.txt'
 ig_html = r'C:\Users\eddyizm\Downloads\eddyizm.html'
-
-# this part will be to scroll to the end and record urls
-
-# browser = webdriver.Chrome()
-# browser.get("https://www.instagram.com/eddyizm")
-# # Selenium script to scroll to the bottom, wait 3 seconds for the next batch of data to load, then continue scrolling.  
-# # It will continue to do this until the page stops loading new data.
-
-# lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-# match=False
-# while(match==False):
-#         lastCount = lenOfPage
-#         time.sleep(10)
-#         lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-#         if lastCount==lenOfPage:
-#             match=True
-            
-# Now that the page is fully scrolled, grab the source code.
-#source_data = browser.page_source
-
-
 URLS = []
 
-#soup = BeautifulSoup(source_data, parse_only=SoupStrainer('a'))
-with open(ig_html, encoding= 'utf-8') as fp:
-    for link in BeautifulSoup(fp, "html.parser", parse_only=SoupStrainer('a') ):
-        if link.has_attr('href'):
-            t = link.get('href')
-            if t is not None:
-              URLS.append(t)
-              print (t)    #if (t[0:2] == '/p/'):
-    
-WriteToArchive(log_path, URLS)
-#browser.close()
-# now to read the file and open the URL, navigate to delete button and remove post,
-# move to new url, wait 10 minutes, delete and loop through a few.
+def stime(seconds):
+    return time.sleep(seconds)
 
 def OpenLog():
     with open(log_path, 'r', encoding= 'utf-8') as g:
         lines = g.read()
         return (lines)
 
-browser = webdriver.Chrome()
-pattern = 'https://www.instagram.com/p/'
+def WriteToArchive(log, data):
+     with open(log, 'w', encoding= 'utf-8') as f:
+         for d in data:
+             f.write(str(d)+'\n')
+
+def parse_href(data, url_list):
+    with open(data, encoding= 'utf-8') as fp:
+        for link in BeautifulSoup(fp, "html.parser", parse_only=SoupStrainer('a') ):
+            if link.has_attr('href'):
+                t = link.get('href')
+                if t is not None:
+                    url_list.append(t)
+                    print (t)
+        return url_list            
+
+# # Selenium script to scroll to the bottom, wait 3 seconds for the next batch of data to load, then continue scrolling.  
+# # It will continue to do this until the page stops loading new data.
+def scroll_to_end():
+    browser = webdriver.Chrome()
+    browser.get("https://www.instagram.com/eddyizm")
+    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    match=False
+    count = 0
+    while(match==False):
+            lastCount = lenOfPage
+            time.sleep(10)
+            lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+            count += 1
+            print (count)
+            if lastCount==lenOfPage:
+                match=True
+                print (count)
+    source = browser.page_source()                       
+    browser.close()
+    return source
 
 
-browser.get("https://www.instagram.com/eddyizm")
+# now to read the file and open the URL, navigate to delete button and remove post,
+# move to new url, wait 10 minutes, delete and loop through a few.
+
+def delete_posts(url_file):
+    browser = webdriver.Chrome()
+    pattern = 'https://www.instagram.com/p/'
+    browser.get("https://www.instagram.com/accounts/login/")
+    stime(3)
+
+delete_posts(URLS)
+
+# source_data = scroll_to_end()
+#soup = BeautifulSoup(source_data, parse_only=SoupStrainer('a'))
+    
+# WriteToArchive(log_path, URLS)    
