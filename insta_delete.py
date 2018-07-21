53cf9f1e7dd85d75761107cf70aefb1bcccb94e3
eddyizm <eddyizm@gmail.com>
2018-07-20 22:17:51 -0700

    fixed parsing bug

--- insta_delete.py
+++ insta_delete.py
@@ -10,8 +10,9 @@ import os
 
 # store urls to delete later
 log_path = 'C:/Users/eddyizm/Source/Repos/seleniumTesting/env/media_urls.txt'
-ig_html = r'C:\Users\eddyizm\Downloads\eddyizm.html'
+# ig_html = r'C:\Users\eddyizm\Downloads\eddyizm.html'
 logintext = "C:\\Users\\eddyizm\\Desktop\\Work\\login.txt"
+URLS = []
 
 def stime(seconds):
     return time.sleep(seconds)
@@ -24,74 +25,42 @@ def OpenLog():
 def WriteToArchive(log, data):
      with open(log, 'w', encoding= 'utf-8') as f:
          for d in data:
-             f.write(str(d)+'\n')
+             f.write('https://www.instagram.com'+str(d)+'\n')
 
 def parse_href(data):
     url_list = []
-    with open(data, encoding= 'utf-8') as fp:
-        for link in BeautifulSoup(fp, "html.parser", parse_only=SoupStrainer('a') ):
-            if link.has_attr('href'):
-                t = link.get('href')
-                if t is not None:
-                    url_list.append(t)
-                    print (t)
-        return url_list            
+    for link in BeautifulSoup(data, "html.parser", parse_only=SoupStrainer('a') ):
+        if link.has_attr('href'):
+            t = link.get('href')
+            print (t)
+            if t is not None:
+                url_list.append(t)
+                
+    return url_list            
 
-# # Selenium script to scroll to the bottom, wait 3 seconds for the next batch of data to load, then continue scrolling.  
-# # It will continue to do this until the page stops loading new data.
 def scroll_to_end():
     browser = webdriver.Chrome()
-    browser.get("https://www.instagram.com/eddyizm")
-    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    match=False
-    count = 0
-    while(match==False):
-            lastCount = lenOfPage
+    get_html = None
+    try:
+        browser.get("https://www.instagram.com/eddyizm")
+        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        match=False
+        count = 0
+        while(match==False):
+        lastCount = lenOfPage
             time.sleep(10)
             lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
             count += 1
-            print (count)
             if lastCount==lenOfPage:
                 match=True
                 print (count)
-    source = browser.page_source()                       
-    browser.close()
-    return source
-
-def delete_posts(browser_object):
-    links = OpenLog()
-    new_file = []
-    deleted_urls = []
-    counter = 10
-    for l in links:
-        if l.startswith('https://www.instagram.com/p/'):
-            new_file.append(l)
-            
-            #browser_object
-    try:
-        while counter > 0:
-            browser_object.get(new_file[counter])
-            stime(10)
-            options_button = browser_object.find_element_by_xpath(
-                "//span[text()='More options']")
-            ActionChains(browser_object).move_to_element(options_button).click().perform()                
-            stime(3)
-            delete_button = browser_object.find_element_by_xpath(
-                "//button[text()='Delete']")
-            ActionChains(browser_object).move_to_element(delete_button).click().perform()
-            stime(10)
-            confirm_delete = browser_object.find_element_by_xpath(
-                "//button[text()='Delete']")
-            ActionChains(browser_object).move_to_element(confirm_delete).click().perform()
-            stime(10)
-            deleted_urls.append(new_file[counter])
-            counter -= 1
-
-        l3 = [x for x in new_file if x not in deleted_urls]
-        WriteToArchive(log_path, l3)	    
-
-    except:
-        pass
+        get_html = browser.page_source                       
+        browser.close()
+    except Exception as err:
+        print (err)
+        browser.close()
+    
+    return get_html
 
 def login_to_site():
     mobile_emulation = { "deviceName": "Pixel 2" }
@@ -172,7 +141,12 @@ def login_to_site():
 if (os.stat(log_path).st_size == 0):
     source_data = scroll_to_end()
     URLS = parse_href(source_data)
+    print (URLS)
     WriteToArchive(log_path, URLS)    
+
+# # manually load html file
+# URLS = parse_href(ig_html)
+# WriteToArchive(log_path, URLS)
 
 login_to_site()
 
