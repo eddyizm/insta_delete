9af032ee32ad6cab62a05616132f7d9012eb13a1
eddyizm <eddyizm@gmail.com>
2018-07-21 18:34:51 -0700

    working through writing urls back to file bug

--- insta_delete.py
+++ insta_delete.py
@@ -5,13 +5,14 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup, SoupStrainer
+from datetime import datetime
 import time
 import os
 import sys
 
 # store urls to delete later
 log_path = 'C:/Users/eddyizm/Source/Repos/seleniumTesting/env/media_urls.txt'
-ig_html = r'C:\Users\eddyizm\Downloads\eddyizm.html'
+#ig_html = r'C:\Users\eddyizm\Downloads\eddyizm.html'
 logintext = "C:\\Users\\eddyizm\\Desktop\\Work\\login.txt"
 URLS = []
 
@@ -24,11 +25,14 @@ def OpenLog():
         return (lines)
 
 def WriteToArchive(log, data):
-     with open(log, 'w', encoding= 'utf-8') as f:
-         for d in data:
-             f.write(str(d)+'\n')
-             #f.write('https://www.instagram.com'+str(d)+'\n')
-
+    with open(log, 'w', encoding= 'utf-8') as f:
+        for d in data:
+            if d.startswith('https://www.instagram.com/'):
+                f.write(str(d)+'\n')
+            else:
+                f.write('https://www.instagram.com'+str(d)+'\n')
+            
+             
 def parse_href(data):
     url_list = []
     for link in BeautifulSoup(data, "html.parser", parse_only=SoupStrainer('a') ):
@@ -43,6 +47,8 @@ def parse_href(data):
 def scroll_to_end():
     browser = webdriver.Chrome()
     get_html = None
+    print (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    print ('scrolling profile to get more urls')
     try:
         browser.get("https://www.instagram.com/eddyizm")
         lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
@@ -58,7 +64,8 @@ def scroll_to_end():
                 
         get_html = browser.page_source                       
         browser.close()
-        print (count)
+        print ('scrolled down: '+str(count)+' times!')
+        print (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     except Exception as err:
         print (err)
         browser.close()
@@ -66,6 +73,8 @@ def scroll_to_end():
     return get_html
 
 def login_to_site():
+    print ('logging in as mobile device to delete')
+    print (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     mobile_emulation = { "deviceName": "Pixel 2" }
     options = webdriver.ChromeOptions()
     options.add_experimental_option("mobileEmulation", mobile_emulation)
@@ -105,15 +114,17 @@ def login_to_site():
     for l in links:
         if l.startswith('https://www.instagram.com/p/'):
             new_file.append(l)
-    
+    print ('counter should be set to 10: '+str(counter))
+    print ('length of file: '+str(len(new_file)))
     if (counter >= len(new_file)):
         counter = (len(new_file) - 1)
-
+    
     for n in new_file:
         print (n)
     print (counter)
     try:
-        print ('in try block')
+        print ('in try block to go into delete while loop')
+        print (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         while (counter > -1):
             print (new_file[counter])
             browser.get(new_file[counter])
@@ -138,6 +149,8 @@ def login_to_site():
                 counter -= 1
 
         l3 = [x for x in new_file if x not in deleted_urls]
+        print ('while loop done and exited successfully')
+        print (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         WriteToArchive(log_path, l3)	    
         browser.close()
 
@@ -145,10 +158,16 @@ def login_to_site():
         print (err)
         browser.close()
         sys.exit()
-   
+
+print ('--------------------------------------- new session ------------------------------------------------- ')
+print (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+file_size = os.stat(log_path).st_size
+print ('file size: '+str(file_size))
 if (os.stat(log_path).st_size == 0):
+    print ('file empty, going to scroll')
     source_data = scroll_to_end()
     URLS = parse_href(source_data)
+    print ('printing parsed URLS to write to log')
     print (URLS)
     WriteToArchive(log_path, URLS)    
 
@@ -157,5 +176,6 @@ if (os.stat(log_path).st_size == 0):
 # WriteToArchive(log_path, URLS)
 
 login_to_site()
-
+print ('--------------------------------------- end session ------------------------------------------------- ')
+print (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 sys.exit()
