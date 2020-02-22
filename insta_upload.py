ea87f679498bd049200543c81804a9d0c417bbd7
eddyizm <cervantes.e@outlook.com>
2020-02-21 20:52:43 -0800

    pushing image successful. next need to process image

--- insta_upload.py
+++ insta_upload.py
@@ -1,16 +1,18 @@
 # -*- coding: UTF-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup, SoupStrainer
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.chrome.options import Options
+# from bs4 import BeautifulSoup, SoupStrainer
 from datetime import datetime
 from glob import glob
+from selenium.webdriver.remote.file_detector import UselessFileDetector
 import time
 import os
 import sys
 from random import randrange
+import pyautogui
 
 ''' writing a script to automate photo uploads '''
 firefoxPath="env/geckodriver"
@@ -40,8 +42,9 @@ def get_images(folder : str):
     foldertag = os.path.basename(os.path.dirname(fullpath))
     return fullpath, foldertag
 
+
 def return_randomtime():
-    return randrange(35)
+    return randrange(5,35)
 
 
 def login_to_site():
@@ -75,20 +78,52 @@ def login_to_site():
         time.sleep(return_randomtime())
         login_button = browser.find_element_by_xpath(
             "//*[contains(text(), 'Log In')]")
-            #"//button[text()='Log In']")
-            #"//form/span/button[text()='Log In']")
-                     
+            
         ActionChains(browser).move_to_element(login_button).click().perform()
         time.sleep(return_randomtime())
+        # skip over save login credentials screen
+        print('login successful, return browser to homepage')
+        browser.get("https://www.instagram.com/")
         return browser
     except Exception as err:
+        print('error in LoginToSite')
         print (err)        
 
 
+def upload_image(browser_object : str, filepath : str):
+    try:
+        print('finding upload image button')
+        browser_object.file_detector = UselessFileDetector()
+        options_button = browser_object.find_element_by_xpath(
+                            "//div[@class='q02Nz _0TPg']//*[@aria-label='New Post']")
+        ActionChains(browser_object).move_to_element(options_button).click().perform()
+        time.sleep(return_randomtime())
+        print('selecting file on local file system')
+        pyautogui.write(filepath, interval=0.25) 
+        pyautogui.press('return')
+        print('file pushed to browser. now to resize and add the tags.')
+        time.sleep(return_randomtime())
+        return browser_object
+        # TODO add method to resize photo and apply a couple of tags, move to next screen and post.                
+    except Exception as ex:
+        print('error in upload_image():', ex)
+        
+
+def process_image(browser_object : str, tags : str):
+    try:
+        time.sleep(return_randomtime())
+        return True
+    except Exception as ex:
+        print('error in process_image():', ex)
+        return False
 
 
+def main():
+    driver = login_to_site()
+    file, tag = get_images(image_path)
+    # print(file, tag)
+    next_driver = upload_image(driver, file)
+    process_image(next_driver, tag)   
 
 if __name__ == '__main__':
-    file, tag = get_images(image_path)
-    print(file, tag)
-    pass
+    main()
