b6158cfa805d44be25ef0d97964faae25cbfe659
eddyizm <cervantes.e@outlook.com>
2020-02-25 22:09:10 -0800

    added bash command to delete empty folders when done.

--- upload.sh
+++ upload.sh
@@ -3,3 +3,5 @@ PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/ed
 cd /home/eddyizm-hp/Documents/insta_delete/ && 
 source /home/eddyizm-hp/Documents/insta_delete/env/bin/activate &&
 python /home/eddyizm-hp/Documents/insta_delete/insta_upload.py 
+
+find /home/eddyizm-hp/HP/images/ -type d -empty -delete
