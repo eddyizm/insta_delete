ad1b4f5724396426345fe216430613e981d5f9ef
eddyizm <eddyizm@gmail.com>
2022-12-15 14:55:20 -0800

    added models file to store settings

---
+++ models.py
@@ -0,0 +1,9 @@
+class Settings:
+    def __init__(self, insta_username, insta_password, log_path, app_log, firefox_path, profile_path, image_path):
+        self.insta_username = insta_username
+        self.insta_password = insta_password
+        self.log_path = log_path
+        self.app_log = app_log
+        self.firefox_path = firefox_path
+        self.profile_path = profile_path
+        self.image_path = image_path
