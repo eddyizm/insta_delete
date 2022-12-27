3a611ed0d0744c3688ac20c1b08f8b01bf9f1e79
eddyizm <eddyizm@gmail.com>
2022-12-26 17:46:53 -0800

    using the like script to test and debug other functions

--- insta_like.py
+++ insta_like.py
@@ -5,19 +5,20 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.remote.file_detector import UselessFileDetector
 from bs4 import BeautifulSoup
 from datetime import datetime
-
-import os
-import logging
-import pyautogui
-import pickle
-
 import insta_base as ib
+import insta_upload as iu
+
+import logging
 
 log = logging.getLogger(__name__)
 
+
 def main():
     log.info(f'file directory: {ib.BASE_DIR}')
-
+    # iu.main()
+    file, tag = iu.get_images(ib.Settings.image_path)
+    log.info(file)
+    log.info(tag)
 
 if __name__ == "__main__":
     main()
