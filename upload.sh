96767eba78224d2b71d933fe34556e7631a5862f
eddyizm <eddyizm@gmail.com>
2023-01-02 16:53:44 -0800

    removed old  commands, need to update

--- upload.sh
+++ upload.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/eddyizm=hp/bin
 find /home/eddyizm-hp/HP/images/ -type d -empty -delete
-# cd /home/eddyizm-hp/Documents/insta_delete/ && 
-# source /home/eddyizm-hp/Documents/insta_delete/env/bin/activate &&
-# python /home/eddyizm-hp/Documents/insta_delete/insta_upload.py 
