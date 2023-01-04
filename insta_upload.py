50eb7d57cd43b654b2571791c3d475c3c85b5190
eddyizm <eddyizm@gmail.com>
2023-01-03 16:56:20 -0800

    changed base import to alias

--- insta_upload.py
+++ insta_upload.py
@@ -12,7 +12,7 @@ from selenium.webdriver.remote.file_detector import UselessFileDetector
 from random import shuffle
 import pyautogui
 
-import insta_base
+import insta_base as ib
 
 log = logging.getLogger(__name__)
 pyautogui.FAILSAFE = False
@@ -34,30 +34,30 @@ def get_images(folder : str):
 def upload_image(browser_object : webdriver, filepath : str):
     try:
         log.info('finding upload image button')
-        insta_base.stime(True)
+        ib.stime(True)
         browser_object.file_detector = UselessFileDetector()
-        if insta_base.check_for_text('Save Your Login Info', browser_object) and insta_base.check_for_text('Turn on Notifications', browser_object):
+        if ib.check_for_text('Save Your Login Info', browser_object) and ib.check_for_text('Turn on Notifications', browser_object):
             browser_object.get(f"https://www.instagram.com/")
         
         new_post_option = browser_object.find_element(by=By.XPATH,
                             value="//*[local-name()='svg' and @aria-label='New post']")
         log.info('found new post option')
         ActionChains(browser_object).move_to_element(new_post_option).click().perform()
-        insta_base.stime(True)
+        ib.stime(True)
         upload_button = browser_object.find_element(by=By.XPATH,
                             value="//button[text()='Select from computer']")
         log.info('found select from computer button')
-        insta_base.stime()
+        ib.stime()
         ActionChains(browser_object).move_to_element(upload_button).click().perform()
         # browser_object.save_screenshot(os.path.join(insta_base.BASE_DIR,'selectingfile.png'))
-        insta_base.stime(True)
+        ib.stime(True)
         log.info(f'selecting file on local file system: {filepath}')
         pyautogui.write(filepath)
         pyautogui.press('tab')
         pyautogui.press('tab')
         pyautogui.press('enter')
         log.info('window explorer tabbed and hit entered to close')
-        insta_base.stime()
+        ib.stime()
         return browser_object
     except Exception as ex:
         browser_object.quit()
@@ -67,21 +67,21 @@ def upload_image(browser_object : webdriver, filepath : str):
 def process_image(browser_object : webdriver, tags : str):
     try:
         log.info('starting process_image')
-        insta_base.stime()
+        ib.stime()
         log.info('looking for next button')
         btn = browser_object.find_element(by=By.XPATH,
                             value="//button[.='Next']")
-        insta_base.stime(True)
+        ib.stime(True)
         if btn:
             log.info('found next button')
             ActionChains(browser_object).move_to_element(btn).click().perform()
         else:
             log.warn('Next button not found...fail!')
             return False
-        insta_base.stime()
+        ib.stime()
         btn = browser_object.find_element(by=By.XPATH,
                             value="//button[.='Next']")
-        insta_base.stime(True)
+        ib.stime(True)
         if btn:
             ActionChains(browser_object).move_to_element(btn).click().perform()
             log.info('found next button, moving to caption screen')
@@ -90,16 +90,16 @@ def process_image(browser_object : webdriver, tags : str):
             return False            
         browser_object.implicitly_wait(10)
         add_text = browser_object.find_elements(by=By.XPATH, value="//*[local-name()='div' and @aria-label='Write a caption...']")[0]
-        insta_base.stime()
+        ib.stime()
         log.info('writing caption')
         ActionChains(browser_object).move_to_element(add_text).click().send_keys(tags).perform()
-        insta_base.stime(True)
+        ib.stime(True)
         log.info('locating share button')
         share_button = browser_object.find_element(by=By.XPATH, value="//button[.='Share']")
-        insta_base.stime()
+        ib.stime()
         log.info('sharing post')
         ActionChains(browser_object).move_to_element(share_button).click().perform()
-        insta_base.stime(True)
+        ib.stime(True)
         log.info('post successful!')
         browser_object.quit()
         return True
@@ -110,20 +110,20 @@ def process_image(browser_object : webdriver, tags : str):
 
 
 def main():
-    insta_base.start_end_log(__file__)
+    ib.start_end_log(__file__)
     attempts = 1
     while attempts > 0:
         attempts = attempts -1
-        driver = insta_base.login_with_cookies()
+        driver = ib.login_with_cookies()
         if not driver:
             log.info('attempt failed. trying again')
-            insta_base.stime(True)
+            ib.stime(True)
             continue
-        file, tag = get_images(insta_base.Settings.image_path)
+        file, tag = get_images(ib.Settings.image_path)
         next_driver = upload_image(driver, file)
         if not next_driver:
             log.info('attempt failed. trying again')
-            insta_base.stime(True)
+            ib.stime(True)
             continue
         combined_tags = f'#{tag} #eddyizm | https://eddyizm.com'
         if process_image(next_driver, combined_tags):
@@ -132,10 +132,10 @@ def main():
             break
         else:
             log.info(f'error posting file.')
-            insta_base.stime(True)
+            ib.stime(True)
             continue
-    insta_base.start_end_log(__file__, True)
+
 
 if __name__ == '__main__':
-    insta_base.stime(True)
     main()
+    ib.start_end_log(__file__, True)
