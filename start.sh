0967a137aba4bdcaa3849707de5b994fb369d65c
eddyizm <eddyizm@gmail.com>
2023-01-04 21:15:05 -0800

    added start shell script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# calling insta scripts via shell file. 
+source <path to your files>/env/Scripts/activate
+# replace with your desired script or add more than one. 
+# BEWARE of IG locking you out if overused. YMMV
+python <path to your files>/insta_delete/insta_like.py 
