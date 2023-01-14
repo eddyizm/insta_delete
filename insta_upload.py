6c2d945631dbd66692098ffa251b5ed3f3f920fe
eddyizm <eddyizm@gmail.com>
2023-01-13 19:35:19 -0800

    cleaned up upload, broke up larger funcs

--- insta_upload.py
+++ insta_upload.py
@@ -1,18 +1,15 @@
 # -*- coding: UTF-8 -*-
 ''' writing a script to automate photo uploads '''
-from glob import glob
 import os
+import pyautogui
 import logging
-
-
+from glob import glob
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.file_detector import UselessFileDetector
-
 from sys import exit
 from random import shuffle
-import pyautogui
 
 import insta_base as ib
 
@@ -37,10 +34,10 @@ def select_local_file(full_file_path):
     """pyautogui actions to select file and close pop up"""
     log.info(f'selecting file on local file system: {full_file_path}')
     pyautogui.write(full_file_path)
-    ib.stime
+    ib.stime()
     pyautogui.press('tab')
     pyautogui.press('tab')
-    ib.stime
+    ib.stime()
     pyautogui.press('enter')
     log.info('window explorer tabbed and hit entered to close')
 
@@ -48,62 +45,50 @@ def select_local_file(full_file_path):
 def upload_image(browser : webdriver, filepath : str):
     try:
         log.info('finding upload image button')
-        ib.stime(True)
+        ib.stime()
         browser.file_detector = UselessFileDetector()
         new_post_option = browser.find_element(by=By.XPATH,
                             value="//*[local-name()='svg' and @aria-label='New post']")
         log.info('found new post option')
-        ActionChains(browser).move_to_element(new_post_option).click().perform()
-        ib.stime(True)
+        ib.click_element(browser, new_post_option, 'new post option')
+        ib.stime()
         upload_button = browser.find_element(by=By.XPATH,
                             value="//button[text()='Select from computer']")
         log.info('found select from computer button')
+        ib.click_element(browser, upload_button, 'upload button')
         ib.stime()
-        ActionChains(browser).move_to_element(upload_button).click().perform()
-        ib.stime(True)
         select_local_file(filepath)
-        ib.stime()
     except Exception as ex:
         browser.quit()
         log.error('error in upload_image():', exc_info=True)
 
 
+def find_next_button(browser):
+    '''find delete button and click!'''
+    log.info(f'finding next button...')
+    ib.stime()
+    next = browser.find_element(by=By.XPATH,
+                            value="//button[.='Next']")
+    log.info('found next button')                            
+    ib.click_element(browser, next, 'next')
+
+
 def process_image(browser_object : webdriver, tags : str):
     try:
         log.info('starting process_image')
+        find_next_button(browser_object)
         ib.stime()
         log.info('looking for next button')
-        btn = browser_object.find_element(by=By.XPATH,
-                            value="//button[.='Next']")
-        ib.stime(True)
-        if btn:
-            log.info('found next button')
-            ActionChains(browser_object).move_to_element(btn).click().perform()
-        else:
-            log.warn('Next button not found...fail!')
-            return False
-        ib.stime()
-        btn = browser_object.find_element(by=By.XPATH,
-                            value="//button[.='Next']")
-        ib.stime(True)
-        if btn:
-            ActionChains(browser_object).move_to_element(btn).click().perform()
-            log.info('found next button, moving to caption screen')
-        else:
-            log.warn('Next button not found...fail!')
-            return False            
-        browser_object.implicitly_wait(10)
+        find_next_button(browser_object)
         add_text = browser_object.find_elements(by=By.XPATH, value="//*[local-name()='div' and @aria-label='Write a caption...']")[0]
-        ib.stime()
         log.info('writing caption')
         ActionChains(browser_object).move_to_element(add_text).click().send_keys(tags).perform()
-        ib.stime(True)
+        ib.stime()
         log.info('locating share button')
         share_button = browser_object.find_element(by=By.XPATH, value="//button[.='Share']")
-        ib.stime()
         log.info('sharing post')
         ActionChains(browser_object).move_to_element(share_button).click().perform()
-        ib.stime(True)
+        ib.stime()
         log.info('post successful!')
         ib.save_cookies(browser_object)
         return True
